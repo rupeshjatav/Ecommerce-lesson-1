606bc5a61875d7ccef55de77af70eedf99c84315
rupeshjatav <rupeshjatav22@gmail.com>
2021-03-02 22:17:49 +0530

    header component was created and links of shop and contact was created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 import Shoppage from "./components/shop/shop.component";
+import Header from './components/header/header.component.jsx';
 
 
 // const HatsPage = () => (
@@ -29,6 +30,7 @@ import Shoppage from "./components/shop/shop.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={Shoppage} />
